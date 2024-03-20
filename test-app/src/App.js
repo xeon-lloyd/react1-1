ac05e6e37314f4c3592a626ce66eebae20e086e1
xeon-lloyd <xeon_lloyd@ritchko.com>
2024-03-20 12:48:46 +0900

    test-app react initial commit

---
+++ test-app/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  // 데이터 전처리는 여기서 파파팍 파파팍
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          흠흐밍은 완전 흠흐밍이네<br></br>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React gogo
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
